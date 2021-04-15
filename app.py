5b3d8c3fd033b2b13bcd4c3f171e2413f7dcd23b
Rahul Adwani <61354796+rahul-adwani@users.noreply.github.com>
2021-04-15 19:31:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def update_value(input_data):
 
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    app.run_server(host='0.0.0.0', port=8081, debug=True)
 
